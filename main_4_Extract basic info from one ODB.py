e7a4def9f63f98745fd5e5f7b4a6fd4070466f98
Javad <104990234+javadkazem@users.noreply.github.com>
2025-07-10 15:59:02 +0200

    Add files via upload

---
+++ main_4_Extract basic info from one ODB.py
@@ -0,0 +1,47 @@
+'''
+* This script should be executed in the Abaqus environment
+
+* this script extract some basic informations from one ODB file
+
+'''
+
+import numpy as np
+import sys
+sys.path.append('d://_CL_Python//')
+from CL_Abaqus_ODB_Reader import *
+CL_ODBR_Say_Hello()
+
+#---------------------------------------------------------- BEGIN
+print('='*10 + '> BEGIN <' + '='*10 + '\n')
+#---------------------------------------------------------- Options
+ODBDir     = 'D:\Rolling\Case-2-Two Rollers\DOE-Simulations\\0'     # The directory contains ODB file
+ODBFName   = 'Jobnew.odb'                                # Name of the ODB file
+#---------------------------------------------------------- open ODB file
+ODBFName = os.path.join(ODBDir, ODBFName)
+print('-'*80 + ' ODB File name:')
+print(ODBFName)
+ODB = CL_ODBR_OpenODB(session, ODBFName)
+#---------------------------------------------------------- All instance names
+InsNames = CL_ODBR_InsNames(ODB)
+print('-'*80 + ' Instance names:')
+for InsName in InsNames:
+    print(InsName)
+#---------------------------------------------------------- All step names
+StepNames = CL_ODBR_StepNames(ODB)
+print('-'*80 + ' Step names:')
+for StepName in StepNames:
+    print(StepName)
+#---------------------------------------------------------- All field output names for all steps
+print('-'*80 + ' Field outputs:')
+for StepName in StepNames:
+    FieldOutputNames = CL_ODBR_FieldOutputNames(ODB,StepName,-1)
+    print('-'*20 + ' ' + StepName)
+    for FieldOutputName in FieldOutputNames:
+        print(FieldOutputName)
+#---------------------------------------------------------- END
+print('='*10 + '> END <' + '='*10 + '\n')
+
+
+
+
+
